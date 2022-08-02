a6c50462bac923a548536b6b5d36abcad0b85106
tigawanna <denniskinuthiaw@gmail.com>
2022-08-02 20:23:34 +0300

    __init__ storage

--- models/base_model.py
+++ models/base_model.py
@@ -4,6 +4,7 @@ contains BaseModel definition
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -11,28 +12,19 @@ class BaseModel:
     defines all common attributes/methods for other classes
     """
 
-    def __init__(self, *args, **kwargs):
-        """initializes object using dictionary if given or it gives default"""
-        if kwargs:
-            for key, value in kwargs.items():
-                if key is not '__class__':
-                    setattr(self, key, value)
-            self.created_at = datetime.strptime(
-                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
-        """"string repr of obj"""
+        """string repr of obj"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all key/value of __dict__
