3bd71f40a35e6d6375c0769fbb8d1f546f41397d
tigawanna <denniskinuthiaw@gmail.com>
2022-08-03 19:58:17 +0300

    all the sub classes

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """
-contains BaseModel definition
+contains BaseModel definitions.
 """
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -12,27 +13,29 @@ class BaseModel:
     """
 
     def __init__(self, *args, **kwargs):
-        """initializes object using dictionary if given or it gives default """
+        """initializes object using dictionary if given or it gives default"""
         if kwargs:
             for key, value in kwargs.items():
-                if key is not '__class__':
+                if key != '__class__':
                     setattr(self, key, value)
-            self.created_at = datetime.strptime(
-                kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
-            self.updated_at = datetime.strptime(
-                kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+            self.created_at = datetime.strptime(kwargs['created_at'],
+                                                '%Y-%m-%dT%H:%M:%S.%f')
+            self.updated_at = datetime.strptime(kwargs['updated_at'],
+                                                '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
-        """"string repr of obj"""
+        """string repr of obj"""
         return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
 
     def save(self):
         """updates the public instance attribute"""
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """returns a dictionary containing all key/value of __dict__
