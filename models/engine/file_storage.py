ca6c2ba9f6815d4dbcbf78fab182f16d4362330e
chrpkorir <faythkoko@gmail.com>
2022-08-06 07:35:43 +0000

    Update storage to be dynamic

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,8 +33,17 @@ class FileStorage:
         """Returns a dictionary of a valid classes and their references."""
 
         from models.base_model import BaseModel
+        from models.user import User
+        from models.place import Place
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.review import Review
 
-        classes = {"BaseModel": BaseModel}
+        classes = {'BaseModel': BaseModel,'User': User, 'Place': Place,
+                'State': State, 'City': City, 'Amenity': Amenity,
+                'Review': Review
+                }
         return classes
 
     def reload(self):
